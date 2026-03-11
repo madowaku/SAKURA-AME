7625e16d5620617014509f0ae3a9481881b0964c
madowaku <cacao.ixora.coccinea@gmail.com>
2026-03-11 22:31:44 +0900

    widjet

---
+++ app/src/main/java/io/github/madobeno/twa/SakuraWidgetProvider.java
@@ -0,0 +1,40 @@
+package io.github.madobeno.twa;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+public class SakuraWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        // ランダムな一言を設定
+        Resources res = context.getResources();
+        String[] quotes = res.getStringArray(R.array.widget_quotes);
+        String quote = quotes[new Random().nextInt(quotes.length)];
+        views.setTextViewText(R.id.widget_quote, quote);
+
+        // タップでアプリを起動する PendingIntent
+        Intent launchIntent = new Intent(context, LauncherActivity.class);
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.widget_root, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
