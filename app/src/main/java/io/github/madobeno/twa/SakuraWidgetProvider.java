c01b6194f4874b6ccd422496d9f850b6714643ba
madowaku <cacao.ixora.coccinea@gmail.com>
2026-03-16 23:28:49 +0900

    widget

--- app/src/main/java/io/github/madobeno/twa/SakuraWidgetProvider.java
+++ app/src/main/java/io/github/madobeno/twa/SakuraWidgetProvider.java
@@ -3,27 +3,52 @@ package io.github.madobeno.twa;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class SakuraWidgetProvider extends AppWidgetProvider {
+    private static final String WIDGET_LAUNCH_PATH = "/SAKURA-AME/";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-
-            Intent intent = new Intent(context, LauncherActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            PendingIntent pendingIntent = PendingIntent.getActivity(
-                    context,
-                    appWidgetId,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
-            );
-
-            views.setOnClickPendingIntent(R.id.widget_root, pendingIntent);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.updateAppWidget(appWidgetId, buildRemoteViews(context));
         }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        refreshAll(context);
+    }
+
+    static void refreshAll(Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName componentName = new ComponentName(context, SakuraWidgetProvider.class);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
+        for (int appWidgetId : appWidgetIds) {
+            appWidgetManager.updateAppWidget(appWidgetId, buildRemoteViews(context));
+        }
+    }
+
+    private static RemoteViews buildRemoteViews(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_sakura_2x1);
+        views.setOnClickPendingIntent(R.id.widget_root, createLaunchPendingIntent(context));
+        return views;
+    }
+
+    private static PendingIntent createLaunchPendingIntent(Context context) {
+        Intent intent = new Intent(context, LauncherActivity.class);
+        intent.putExtra(LauncherActivity.EXTRA_LAUNCH_PATH, WIDGET_LAUNCH_PATH);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        return PendingIntent.getActivity(
+                context,
+                2001,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
     }
 }
